0c82caa96961cfe0cb5cb8f8163c6fd5e7e44f07
Sam Keen <sam.sjk@gmail.com>
2023-06-09 14:05:49 -0700

    v1 is working on PDFs

--- app.py
+++ app.py
@@ -1,34 +1,72 @@
 import streamlit as st
-import langchain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.callbacks import get_openai_callback
+from PyPDF2 import PdfReader
+from dotenv import load_dotenv
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+load_dotenv()
 
 # Define the main function
 def main():
-  # Upload the file
+  
   file = st.file_uploader("Upload your file", type=["pdf", "txt"])
-
-  # If the user has uploaded a file, then proceed
+  # Upload the file
   if file is not None:
-    # Load the file into Langchain
-    document = langchain.Document(file)
+    
+    if "doc_search" not in st.session_state:
+      process_file(file)
 
-    # Create a chat session
-    session = langchain.ChatSession(document)
-
-    # Start the chat
-    while True:
-      # Get the user's input
-      input = st.text_input("Enter your question")
-
-      # If the user has entered a question, then ask Langchain for an answer
-      if input != "":
-        answer = session.ask(input)
-
-        # Display the answer to the user
-        st.write("Answer:", answer)
+    # show user input
+    user_question = st.text_input("Ask a question about your PDF:")
+    if user_question:
+      docs = st.session_state.doc_search.similarity_search(user_question)
+      
+      llm = OpenAI()
+      chain = load_qa_chain(llm, chain_type="stuff")
+      with get_openai_callback() as cb:
+        response = chain.run(input_documents=docs, question=user_question)
+        print(cb)
+          
+      st.write(response)
 
   # If the user has not uploaded a file, then display an error message
   else:
     st.error("Please upload a file")
+
+
+def get_pdf_text(pdf):
+  pdf_reader = PdfReader(pdf)
+  text = ""
+  for page in pdf_reader.pages:
+    text += page.extract_text()
+  return text
+
+def process_file(file):
+  # If the user has uploaded a file, then proceed
+  document_text = ""
+  if file is not None:
+    # check the file type
+    if file.type == "application/pdf":
+      document_text = get_pdf_text(file)
+    else:
+      document_text = file.read().decode("utf-8")
+    # @TODO write document text to a file
+
+  
+  chunks = text_splitter.split_text(document_text)
+
+  # Create a metadata for each chunk
+  metadatas = [{"source": f"{i}-pl"} for i in range(len(chunks))]
+
+  # Create a Chroma vector store
+  embeddings = OpenAIEmbeddings()
+  st.session_state.doc_search = Chroma.from_texts(chunks, embeddings, metadatas=metadatas)
+  
 
 # Run the main function
 if __name__ == "__main__":
